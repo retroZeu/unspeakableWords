0d171e58c007178015dd094007982fb2b71f4396
Miranda Cheung <spcafan1@gmail.com>
2017-05-08 14:00:24 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -12,16 +12,39 @@ public class Game
         discards.addCard(person.getHand().move(card));
     }
     
-    public static void play(Player person, String aWord, FileManager bot, Deck main, Deck discards)
+    public static void play(Player person, String word, FileManager bot, Deck main, Deck discards)
     {
         //1.Check if word is in player's hand. If true, then check if legal(word) or player is insane. 
         //2.If false, end method and try again,
         //3.If true, then add card value to a sum, discard the cards from hand, add sum to player's points
         //4.Sanity check: if d20 roll < word sum, then player loses a sanity
         //5.Draw back to 7.
-        
-    }
-    
+        boolean wordInHand = true;
+        ArrayList<String> letters = new ArrayList<String>();
+        for (int i = 0; i < word.length()-1; i++)
+        {
+            letters.add(word.substring(i, i+1));
+        }
+        while(wordInHand)
+        {
+            for(String check: letters)
+            { 
+                boolean valid = false;
+                while(!valid)
+                {
+                    for(String inHand: Hand.getCards())
+                    {
+                        if(inHand.equals(check.toUppercase()))
+                        {
+                            valid = true;
+                        }
+                    }
+                }
+                wordInHand = valid;
+            }
+        }
+  }
+    public boolean 
     public static void pass(Player person, Deck main, Deck discards)
     {
         //1.Disard hand, draw back up to 7.
@@ -36,3 +59,6 @@ public class Game
         else {return false;}
     }
 }
+
+
+
